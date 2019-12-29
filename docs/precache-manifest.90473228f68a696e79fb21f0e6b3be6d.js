baefe7b392af8717b5ff5fcea7c5f14d19b63caa
Alexander Paz <alexjpaz@gmail.com>
2019-12-29 09:32:49 -0500

    Adding postbuild move to docs for gh-pages

---
+++ docs/precache-manifest.90473228f68a696e79fb21f0e6b3be6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaa110dcdf6e9c076981453c05effef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0351c5b71386be0482a4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba5d8d4b821e9df7d828",
+    "url": "/static/js/2.df0bf385.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.df0bf385.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0351c5b71386be0482a4",
+    "url": "/static/js/main.427dad67.chunk.js"
+  },
+  {
+    "revision": "d14f059ed4b5cf6dfdba",
+    "url": "/static/js/runtime-main.aa6c822b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
