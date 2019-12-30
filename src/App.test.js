f744ee9db5ad9e30d9a6528da4ea08f04cf9cc9a
Alexander Paz <alexjpaz@gmail.com>
2019-12-30 08:58:57 -0500

    fix text

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/The apple is red/i);
   expect(linkElement).toBeInTheDocument();
 });
