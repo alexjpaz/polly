9c9befb979c247c16190e4fe7ae806e2bbe0683f
Alexander Paz <alexjpaz@users.noreply.github.com>
2019-12-31 15:38:23 -0500

    Update GitHubLink.js

--- src/components/GitHubLink.js
+++ src/components/GitHubLink.js
@@ -5,7 +5,7 @@ import './GitHubLink.css';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
-const projectURL = "https://github.com/alexjpaz-playground/polly";
+const projectURL = "https://github.com/alexjpaz/polly";
 
 export const GitHubLink = () => (
   <IconButton href={projectURL} target={"_blank"}>
