8f880635bb75fb2e57d0885568e0674227b98a2c
Alexander Paz <alexjpaz@gmail.com>
2020-03-10 09:50:52 -0400

    Working on getting other sources in the diaglog

--- src/components/Workbook/LoadWorkbookDialog.js
+++ src/components/Workbook/LoadWorkbookDialog.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 import { AppContext } from '../../AppContext';
 import { WorkbookFileField } from './WorkbookFileField';
+
+export function WorkbookSources({ setWorkbookData }) {
+  const [ sourceType, setSourceType ] = React.useState(null);
+
+  const onSourceChange = () => {
+  };
+
+  return (
+    <React.Fragment>
+      <TextField
+          id="workbook-source-selector"
+          select
+          label="Source"
+          value={''}
+          onChange={onSourceChange}
+          helperText="Please select your workbook source"
+        >
+        <MenuItem key={"file"} value={"file"}>
+          File
+        </MenuItem>
+        <MenuItem key={"file"} value={"file"}>
+          File
+        </MenuItem>
+      </TextField>
+      <Box m={1}>
+        <WorkbookFileField onClose={setWorkbookData}/>
+      </Box>
+    </React.Fragment>
+  );
+}
 
 export function LoadWorkbookDialog({ isOpen = false, onClose }) {
   const ctx = React.useContext(AppContext);
@@ -33,7 +66,7 @@ export function LoadWorkbookDialog({ isOpen = false, onClose }) {
           <DialogContentText>
             Please specify a source for your workbook.
           </DialogContentText>
-          <WorkbookFileField onClose={setWorkbookData}/>
+          <WorkbookSources setWorkbookData={setWorkbookData} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCancel} color="default" data-testid="cancel" >
