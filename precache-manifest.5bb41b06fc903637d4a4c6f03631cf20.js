00de6331cdc3d5a44dbd9e4070086a904c7ffd40
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-12 17:43:47 +0000

    Deploy alexjpaz/polly to github.com/alexjpaz/polly.git:gh-pages

---
+++ precache-manifest.5bb41b06fc903637d4a4c6f03631cf20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426fdae3d0d98ddffa35cf9b24d5bb8d",
+    "url": "/polly/index.html"
+  },
+  {
+    "revision": "36e89d7d07e00c88d418",
+    "url": "/polly/static/css/main.e0a32332.chunk.css"
+  },
+  {
+    "revision": "5f5abbe1c6ad9a387e6c",
+    "url": "/polly/static/js/2.cc8fcdcb.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/polly/static/js/2.cc8fcdcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36e89d7d07e00c88d418",
+    "url": "/polly/static/js/main.c6e36213.chunk.js"
+  },
+  {
+    "revision": "961700724f111af0f264",
+    "url": "/polly/static/js/runtime-main.d308d31c.js"
+  },
+  {
+    "revision": "2fd840b805885d7bf50aaf4546fd6563",
+    "url": "/polly/static/media/logo192.2fd840b8.png"
+  }
+]);
